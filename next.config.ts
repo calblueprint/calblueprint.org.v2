875e77028be6b4b768c4e7d02399afde59208d89
Aditya Pawar <apawar@berkeley.edu>
2025-01-16 23:21:22 -0800

    Fix base path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '/' + (process.env.NEXT_PUBLIC_BASE_PATH || ''),
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
