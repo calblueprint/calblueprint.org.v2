2c0ea78b4f4bd06c072eaf814c5b1920df29006e
Aditya Pawar <apawar@berkeley.edu>
2025-01-16 22:37:32 -0800

    Add base path

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  basePath: process.env.BASE_PATH || '/calblueprint.org.v2',
   images: {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
