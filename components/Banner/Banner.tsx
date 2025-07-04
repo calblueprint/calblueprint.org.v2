0ec331948b61594278fbde31d71725cab64e2619
Kyle Ramachandran <kyleramachandran@berkeley.edu>
2025-07-04 11:55:09 +0800

    created infosession banner

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,17 +1,22 @@
-import Link from 'next/link';
+// import Link from 'next/link';
+import Markdown from 'react-markdown';
 import config from '@/data/students_applications.json';
 
+// Banner is temporarily used to display infosession information for nonprofits
+// still uses the students_applications.json file for now, will revert when the time comes
 export default function Banner() {
   return (
-    <Link
-      className="notify-bar-link"
-      href="https://calblueprint.org/apply/students"
-    >
+    // <Link
+    //   className="notify-bar-link"
+    //   href="https://calblueprint.org/apply/students"
+    // >
+    <div>
       <div className="notify-bar-container">
-        <p>
+        {/* <p>
           <span style={{ fontSize: 16 }}>{config.banner_text}</span>
-        </p>
+        </p> */}
+        <Markdown className="text-[16px]">{config.banner_text}</Markdown>
       </div>
-    </Link>
+    </div>
   );
 }
