8076cee2164793429650762d3cd1efd0e851802b
Neha <neha_h@berkeley.edu>
2026-08-23 19:11:40 -0700

    updated design timelien

--- app/(hero-layout)/apply/students/page.tsx
+++ app/(hero-layout)/apply/students/page.tsx
@@ -39,7 +39,7 @@ export default function StudentApplyPage() {
 
   const sections = [
     { text: 'Developer', href: '#developer' },
-    // { text: 'External', href: '#external' }, not recruiting for external in fall 26
+    { text: 'External', href: '#external' },
     { text: 'Designer', href: '#designer' },
     { text: 'Decision Criteria', href: '#decision-criteria' },
     { text: 'FAQs', href: '#faqs' },
@@ -177,11 +177,14 @@ export default function StudentApplyPage() {
         </div>
 
         {/* Not recruiting for external in Fall 2026 */}
-
-        {/* <div className="container mx-auto px-[12px] mt-[54px]" id="external"> 
+        <div className="container mx-auto px-[12px] mt-[54px]" id="external">
           <h2 className="font-bptext text-[28px] sm:text-[36px] font-semibold text-center mb-[16px]">
             External Application Process
           </h2>
+          <p className="text-dark-gray/60 text-[18px] sm:text-[21px] text-center mb-[15px] italic">
+            Due to limited capacity, we will not be recruiting for our external
+            team this semester.
+          </p>
           <p className="text-dark-gray/60 text-[18px] sm:text-[21px] text-center mb-[32px]">
             Learn what it means to be on external{' '}
             <strong>
@@ -241,9 +244,9 @@ export default function StudentApplyPage() {
                     />
                   ) : (
                     <div className="text-bp-blue text-[14px] font-medium">
-                      Change displayed text depending on whether application cycle hasn't started/has passed
+                      {/* Change displayed text depending on whether application cycle hasn't started/has passed
                       Student applications will be open on{' '}
-                      {config.application_release_date}.
+                      {config.application_release_date}. */}
                       Student applications closed on{' '}
                       {config.application_deadline}.
                     </div>
@@ -278,7 +281,7 @@ export default function StudentApplyPage() {
               last
             />
           </div>
-        </div> */}
+        </div>
 
         <div className="container mx-auto px-[12px] mt-[54px]" id="designer">
           <h2 className="font-bptext text-[28px] sm:text-[36px] font-semibold text-center mb-[16px]">
@@ -390,8 +393,7 @@ export default function StudentApplyPage() {
                   Building on top of your work from the interview, this
                   submission will give you a chance to showcase your ability to
                   iterate and your visual design skills. The prompt will be
-                  covered during the interview. We will host design challenge
-                  office hours on {config.design_challenge_office_hours}.
+                  covered during the interview.
                 </>
               }
               last
